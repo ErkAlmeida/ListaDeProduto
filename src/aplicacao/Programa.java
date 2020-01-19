8136fb59dd49f81228db6e555fd4c6a6c4e36f25
Erick Almeida <labsetplus@outlook.com>
2020-01-19 14:00:41 -0300

    Menu de entrada e contrutor criado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,39 @@
 package aplicacao;
 
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Produto;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("Digite o numero de produtos:");
+		int qtProduto = sc.nextInt();
+		
+		for(int i = 0 ; i < qtProduto; i++) 
+		{
+			System.out.printf("Digite o #" + i);
+			System.out.println();
+			
+			sc.nextLine();
+			
+			System.out.println("Digite o nome:");
+			String nome = sc.nextLine();
+			
+			System.out.println("Digite Preço:");
+			double preco = sc.nextDouble();
+			
+			Produto produto = new Produto();
+		}
+		
+		sc.close();
 	}
 
 }
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -1,5 +1,22 @@
 package entidades;
 
-public class Produto {
-
+public class Produto 
+{
+	private String nome;
+	private Double preco;
+	
+	public Produto() 
+	{
+			
+	}
+	
+	public Produto(String nome, Double preco) 
+	{
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	
+	
+	
 }
